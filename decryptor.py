ff63029f4af54af2d86b9bcd29a7906af625a514
lgfranco22 <39537987+lgfranco22@users.noreply.github.com>
2021-04-20 17:36:56 -0300

    update salt
    agora o salt é separado automaticamente

--- decryptor.py
+++ decryptor.py
@@ -5,18 +5,18 @@ if len(sys.argv) != 2:
 	print('Modo de uso: ./'+sys.argv[0]+' wordlist')
 else:
 	print('#############################################')
-	print("# Password Salt Resolver for Linux v1.0    .#")
+	print("# Password Salt Resolver for Linux v1.20   .#")
 	print("# Dev. Luiz G F Michelmann                 .#")
 	print("# Site: https://www.francosinformatica.com .#")
 	print('#############################################\n')
 
-
-	#salt
-	salt = raw_input('Digite o salt: ')
-
 	#senha completa
-	senhaC = raw_input('Digite a senha: ')
-
+	senhaC = raw_input('Digite o hash completo: ')
+	
+	#salt
+	salt_puro = senhaC.split('$')
+        salt = "$"+salt_puro[1]+"$"+salt_puro[2]+"$"
+	
 
 	wordlist = open(sys.argv[1], 'r')
 
